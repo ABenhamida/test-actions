2f034188bacf42c432d3b273f7cd9cec07149362
ABenhamida <ABenhamida@users.noreply.github.com>
2020-04-14 09:37:47 +0100

    Update entrypoint.sh

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -19,7 +19,7 @@ if [ -f "Gruntfile.js" ]
 then
   npm install -g grunt-cli
   echo "Running Grunt with args"
-  grunt $* | reviewdog -efm="%f:%l:%c %m" -name="grunt-custom" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
+  grunt $* > reviewdog -efm="%f:%l:%c %m" -name="grunt-custom" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
 else
   echo "Running NPM with args"
   sh -c "npm $*"
