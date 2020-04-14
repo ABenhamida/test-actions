f290bb23ff054aa7bfee4942623c40074bc92f82
ABenhamida <ABenhamida@users.noreply.github.com>
2020-04-14 11:04:57 +0100

    Update entrypoint.sh

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,29 +1,4 @@
 #!/bin/sh
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-if [ "$DEBUG" == "false" ]
-then
-  # Carry on, but do quit on errors
-  set -e
-else
-  # Verbose debugging
-  set -exuo pipefail
-  export LOG_LEVEL=debug
-  export ACTIONS_STEP_DEBUG=true
-fi
-
-
-echo "Installing NPM dependencies"
-npm install
-
-if [ -f "Gruntfile.js" ]
-then
-  npm install -g grunt-cli
-  echo "Running Grunt with args"
-  #sh -c "grunt $*"
-  #scss_lint
-  scss-lint | reviewdog -efm="%f:%l:%c %m" -name="grunt-custom" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
-else
-  echo "Running NPM with args"
-  sh -c "npm $*"
-fi
+scss-lint | reviewdog -efm="%f:%l:%c %m" -name="grunt-custom" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
