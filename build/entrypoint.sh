feb1ad5024133edb5848cc26f1135bed53881db6
ABenhamida <ABenhamida@users.noreply.github.com>
2020-04-14 10:45:16 +0100

    Update entrypoint.sh

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -21,8 +21,7 @@ then
   echo "Running Grunt with args"
   #sh -c "grunt $*"
   #scss_lint
-  scss-lint
-  #| reviewdog -efm="%f:%l:%c %m" -name="grunt-custom" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
+  scss-lint | reviewdog -efm="%f:%l:%c %m" -name="grunt-custom" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
 else
   echo "Running NPM with args"
   sh -c "npm $*"
